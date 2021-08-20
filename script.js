811110bd5f898f0e8d31e171220690c054d51755
adam-belahouane <adam_belahouane@outlook.com>
2021-08-20 15:38:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
   
-let x = "John";
-let y = "Doe";
+let x = "John"
+let y = "Doe"
 
-console.log(x + "<>" + y);
+console.log(x + "<>" + y)
 
 const me = {
     name:"Adam",
@@ -11,64 +11,63 @@ const me = {
 }
 delete me.email
 
-let array = [];
+let array = []
 
 for (let i = 1; i <= 10; i++) {
-  array.push("string" + i);
+  array.push("string" + i)
 }
 console.log(array)
 
 for (let str of array) {
-    console.log(str);
+    console.log(str)
   }
 
 let randomNumbers = [];
 for (let i = 1; i <= 100; i++) {
-  randomNumbers.push(Math.floor(Math.random() * 100 + 1));
+  randomNumbers.push(Math.floor(Math.random() * 100 + 1))
 }
 
-console.log(randomNumbers);
+console.log(randomNumbers)
 
 function getMaxMin(numbers) {
-    let max = numbers[0];
-    let min = numbers[0];
+    let max = numbers[0]
+    let min = numbers[0]
   
     for (let number of numbers) {
-      max = number > max ? number : max;
-      min = number < min ? number : min;
+      max = number > max ? number : max
+      min = number < min ? number : min
     }
   
-    return { max, min };
+    return { max, min }
   }
   
   console.log(getMaxMin(randomNumbers));
 
-  let arrayOfArrays = [];
+  let arrayOfArrays = []
 
 for (let i = 0; i < 10; i++) {
-  let innerArray = [];
+  let innerArray = []
   for (let j = 0; j < 10; j++) {
-    innerArray.push(Math.floor(Math.random() * 10 + 1));
+    innerArray.push(Math.floor(Math.random() * 10 + 1))
   }
-  arrayOfArrays.push(innerArray);
+  arrayOfArrays.push(innerArray)
 }
 
 function longestArray(array1, array2) {
-    return array1.length >= array2.length ? array1 : array2;
+    return array1.length >= array2.length ? array1 : array2
   }
-  console.log(returnLongest([1, ], [1, 2]));
   
   function Sum(arr1, arr2) {
-    let sum1 = 0;
+    let sum1 = 0
   
     for (let number of arr1) {
-      sum1 += number;
+      sum1 += number
     }
   
-    let sum2 = 0;
+    let sum2 = 0
   
     for (let number of arr2) {
-      sum2 += number;
+      sum2 += number
     }
   
     return sum1 >= sum2 ? sum1 : sum2;
@@ -81,4 +80,22 @@ function longestArray(array1, array2) {
 
   const allTd = document.querySelectorAll("td")
 
-  
+  const changeH1 = function (newH1) {
+    const h1 = document.querySelector("h1");
+    h1.innerText = newH1
+  };
+
+  const createNewRow = function(){
+      newRowNode = document.createElement("tr")
+      table = document.querySelector("tbody")
+      table.appendChild(newRowNode)
+      for (let i = 0; i < 5; i++) {
+        const td = document.createElement("td");
+        td.innerText = i + 1;
+        newRowNode.appendChild(td)
+      }
+  }
+
+  const addClassToRow = function(){
+
+  }
